3e27fc3504d1bd1d64d46fa8a02574a3965aa597
Daniel Del Percio <daniel.t.delpercio@gmail.com>
2022-04-16 15:26:04 +0200

    Refactor into packages

--- flaskrest/product.py
+++ flaskrest/product.py
@@ -5,8 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 
-#Init app
-app = Flask(__name__)
+from flaskrest import app
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
