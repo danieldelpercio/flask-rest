1adf0176cdc32482b3b968d4b4f67f79c0831654
Daniel Del Percio <daniel.t.delpercio@gmail.com>
2022-04-16 18:38:34 +0200

    Fix style with flake8

--- flaskrest/__init__.py
+++ flaskrest/__init__.py
@@ -3,24 +3,27 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-#Init app
+# init app
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-#db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'db.sqlite')
+# db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
+    basedir, 'db.sqlite'
+    )
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # init database
 db = SQLAlchemy(app)
 
-#init marshmallow
+# init marshmallow
 ma = Marshmallow(app)
 
 
 import flaskrest.product
 import flaskrest.weather
+
 
 @app.route('/')
 def index():
--- flaskrest/weather.py
+++ flaskrest/weather.py
@@ -1,15 +1,12 @@
-import os,requests
+import requests
 
-from flask import Flask,jsonify,request
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
+from flask import jsonify, request
 
 
-from flaskrest import app,db,ma
+from flaskrest import app, db, ma
 
 
-
-#Weather Class/Model
+# Weather Class/Model
 class Weather(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
@@ -19,29 +16,31 @@ class Weather(db.Model):
     coord_lan = db.Column(db.Float)
     icon = db.Column(db.String(10))
 
-    def __init__(self,name):
-        self.name=name
+    def __init__(self, name):
+        self.name = name
 
-    # def __init__(self, name, description,temp,coord_lon,coord_lan,icon):
-    #     self.name = name
-    #     self.description = description
-    #     self.temp = temp
-    #     self.coord_lon = coord_lon
-    #     self.coord_lan = coord_lan
-    #     self.icon = icon
 
-#Weather schema
+# Weather schema
 class WeatherSchema(ma.Schema):
     class Meta:
-        fields = ('id','name','description','temp','coord_lon','coord_lan','icon')
+        fields = (
+            'id',
+            'name',
+            'description',
+            'temp',
+            'coord_lon',
+            'coord_lan',
+            'icon'
+            )
 
-#init schema
+
+# init schema
 weather_schema = WeatherSchema()
 weathers_schema = WeatherSchema(many=True)
 
 
-#Add a city
-@app.route('/city',methods=['POST'])
+# Add a city
+@app.route('/city', methods=['POST'])
 def add_city():
     name = request.json['name']
 
@@ -54,40 +53,44 @@ def add_city():
 
     return weather_schema.jsonify(new_city)
 
-#update weather data from API
-@app.route('/weather',methods=['POST'])
-def update_weather_data(): 
+
+# update weather data from API
+@app.route('/weather', methods=['POST'])
+def update_weather_data():
     city = request.json['name']
 
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=1dbaf7baf220c461d9be1a59aebba09a'
-    
+
     r = requests.get(url.format(city)).json()
-    
+
     print(r)
 
-    new_weather=Weather.query.filter_by(name=city).first()
-    new_weather.name=city
-    new_weather.temp=r['main']['temp']
-    new_weather.description=r['weather'][0]['description']
+    new_weather = Weather.query.filter_by(name=city).first()
+    new_weather.name = city
+    new_weather.temp = r['main']['temp']
+    new_weather.description = r['weather'][0]['description']
     db.session.commit()
 
     return weather_schema.jsonify(new_weather)
 
-#Get all weather data
+
+# Get all weather data
 @app.route('/weather', methods=['GET'])
 def get_weathers():
     all_weathers = Weather.query.all()
     result = weathers_schema.dump(all_weathers)
     return jsonify(result)
 
-#Get weather data for specific id
+
+# Get weather data for specific id
 @app.route('/weather/<id>', methods=['GET'])
 def get_weather(id):
     weather = Weather.query.get(id)
     return weather_schema.jsonify(weather)
 
-#Update a Weather
-@app.route('/weather/<id>',methods=['PUT'])
+
+# Update a Weather
+@app.route('/weather/<id>', methods=['PUT'])
 def update_weather(id):
 
     weather = Weather.query.get(id)
@@ -105,17 +108,15 @@ def update_weather(id):
     weather.coord_lon = coord_lon
     weather.icon = icon
 
-
     db.session.commit()
 
     return weather_schema.jsonify(weather)
 
 
-#Delete a Weather
+# Delete a Weather
 @app.route('/weather/<id>', methods=['DELETE'])
 def delete_weather(id):
     weather = Weather.query.get(id)
     db.session.delete(weather)
     db.session.commit()
     return weather_schema.jsonify(weather)
-
